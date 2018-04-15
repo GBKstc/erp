875c06ecc07c838beea0a29bd3d6b3e36f647227
gbk <271677538@qq.com>
2018-04-15 21:50:45 +0800

    edit DynamicFieldSet

--- erp/src/components/BaseUI/DynamicFieldSet.js
+++ erp/src/components/BaseUI/DynamicFieldSet.js
@@ -1,10 +1,18 @@
-import { Form, Input, Icon, Button, InputNumber, Row, Select  } from 'antd';
+import { Form, Input, Icon, Button, InputNumber, Row, Select ,message } from 'antd';
 import { connect } from 'dva';
 import React from 'react'
 import {variable} from "../../utils"
 const FormItem = Form.Item;
 const Option = Select.Option;
 const { isEmpty } = variable;
+
+const helpInfo = {
+  "same":"不能选同一个人",
+  "name":"请选择名称",
+  "total":"比例总和不为100%,请修改",
+  "zero":"比例不能为0"
+
+};
 class DynamicFieldSet extends React.Component{
 
   constructor(props) {
@@ -12,119 +20,87 @@ class DynamicFieldSet extends React.Component{
     this.onChange = this.onChange.bind(this);
     this.add = this.add.bind(this);
     this.onBlur = this.onBlur.bind(this);
+    this.verifyFrom = this.verifyFrom.bind(this);
+    this.setNewChange = this.setNewChange.bind(this);
     this.state = {
       formValue:[
         {
-          name:undefined,
+          name:"",
           value:100,
         }
       ],
-      help:null,
+      help:"",
     };
-    this.formData=[];
-  }
-
-  componentWillReceiveProps (newProps){
-    let formValue = [
-      {
-        name:undefined,
-        value:100,
-      }];
   }
 
 
 
-  onChange(index,e){
+
+  onChange(index,key,value){
     let {formValue,help} = this.state;
-    if(Object.prototype.toString.call(e)==="[object Number]"){
-      formValue[index].value = e;
-    }else if(Object.prototype.toString.call(e)==="[object String]"){
-      help = "";
-      for(let i=0;i<formValue.length;i++){
-        if(formValue[i].name == e){
-          help = "不能选同一个人";
-          break;
-        }
-      }
-      formValue[index].name = e;
-
-    }else{
-      return ;
-    }
-    this.formData = formValue;
+    formValue[index][key] = value;
     let newState = Object.assign({},{formValue},{help});
-    console.log(newState);
-    this.setState({
-      formValue,
-      help
-    });
-    if(this.props.onChange){
-      this.props.onChange(newState);
-    }
+    this.setNewChange(newState);
   }
 
   add(){
-    let {formValue} = this.state;
+    let {formValue,help} = this.state;
     formValue.push(
       {
-        name:undefined,
+        name:"",
         value:0
       }
     );
-    this.setState({
-      formValue
-    });
-    this.formData = formValue;
-    this.onBlur();
+    let newState = Object.assign({},{formValue,help});
+    this.setNewChange(newState);
+
   }
 
   delete(index){
-    let {formValue} = this.state;
+    let {formValue,help} = this.state;
     formValue.splice(index,1);
-    this.setState({
-      formValue
-    });
-    this.formData = formValue;
-    this.onBlur();
+    let newState = Object.assign({},{formValue,help});
+    this.setNewChange(newState);
   }
 
   onBlur(){
-    let formValue = this.formData;
-    let num = 0;
-    for(let i=0;i<formValue.length;i++){
-      if(!formValue[i].name){
-        this.setState({
-          help:"请选择名称",
-        });
-        let newState = Object.assign({},formValue,{help:"请选择名称"});
-        if(this.props.onChange){
-          this.props.onChange(newState);
-        }
-        return ;
-      }
-      num = num + formValue[i].value;
-    }
-    if(num!==100){
-      this.setState({
-        help:"比例总和不为100%,请修改",
-      });
-      let newState = Object.assign({},{formValue},{help:"比例总和不为100%,请修改"});
-      if(this.props.onChange){
-        this.props.onChange(newState);
-      }
-      return ;
-    }
-
-    this.setState({
-      help:"",
-    });
-    let newState = Object.assign({},formValue,{help:""});
-    if(this.props.onChange){
-      this.props.onChange(newState);
-    }
 
   }
 
+  verifyFrom(newState){
+    let { formValue, help} = newState;
+    let allName = "";
+    let allNum = 0;
+    let info = "";
+    for(let i=0;i<formValue.length;i++){
+
+      if(formValue[i].value==0){
+        info = helpInfo.zero;
+      }
+      if(allName.indexOf(formValue[i].name)>-1){
+        info = helpInfo.same;
+      }
+      allName = allName + formValue[i].name;
+      allNum = allNum + formValue[i].value;
+    }
+
+    if(allNum!=100){
+      info = helpInfo.total
+    }
+    help = info;
+    return Object.assign({},{formValue,help});
+  }
+
+  setNewChange(newState){
+    let State = this.verifyFrom(newState);
+    this.setState({
+      ...State
+    });
+    if(this.props.onChange){
+      this.props.onChange(State);
+    }
+
+  }
   render() {
     const {options} = this.props;
     //const options = [
@@ -156,7 +132,7 @@ class DynamicFieldSet extends React.Component{
               optionFilterProp="children"
               style={{ width: '65%', marginRight: '3%'}}
               value={name}
-              onChange={this.onChange.bind(this,index)}
+              onChange={this.onChange.bind(this,index,"name")}
               onBlur={this.onBlur}
               filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
             >
@@ -168,7 +144,7 @@ class DynamicFieldSet extends React.Component{
               max={100}
               formatter={value => `${value}%`}
               parser={value => value.replace('%', '')}
-              onChange={this.onChange.bind(this,index)}
+              onChange={this.onChange.bind(this,index,"value")}
               onBlur={this.onBlur}
             />
             {
